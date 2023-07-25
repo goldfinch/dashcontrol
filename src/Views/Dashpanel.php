25f7d404a976a668f7c76e0e4600eba919c0ea59
Art <>
2023-07-25 15:01:15 +1200

    move core components

--- src/Views/Dashpanel.php
+++ src/Views/Dashpanel.php
@@ -4,7 +4,7 @@ namespace Goldfinch\Dashpanel\Views;
 
 use SilverStripe\View\ViewableData;
 use SilverStripe\Security\Permission;
-use Goldfinch\Dashpanel\Services\PanelService;
+use Goldfinch\Dashcore\Services\DashService;
 
 class Dashpanel extends ViewableData
 {
@@ -15,7 +15,7 @@ class Dashpanel extends ViewableData
             return;
         }
 
-        $data = PanelService::getPanelInitialData();
+        $data = DashService::getPanelInitialData();
 
         return $this->customise(['jsonData' => $data ? json_encode($data) : null])->renderWith('Goldfinch/Dashpanel/Views/Dashpanel');
     }
